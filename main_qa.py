fddb2b96ab0ae1868e2419a689bd30100e8aa0b6
G-iarrusso <g.iarrusso.dev@gmail.com>
2021-01-11 21:13:09 -0500

    added a POC to main_qa

--- main_qa.py
+++ main_qa.py
@@ -1,3 +1,24 @@
 from subprocess import call
 call(["python", "test.py"])
 input("youve made it back?")
+"""
+TODO
+add a dict for #:test file
+add a list of suites: PR, Daily, regression
+pseudo code:
+tests/regression = {
+    1: "test_TC1_name"
+    .
+    .
+    .
+}
+Daily = {
+    base of code that people need to run
+}
+PR = {
+    base of code to run on a PR
+}
+can expand to targeted regression i.e. movement, attacking, scavenging
+
+run each test
+"""
