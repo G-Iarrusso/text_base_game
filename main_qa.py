c77a218092916e06cbf19a3a5c821fb6cc7d55b4
G-iarrusso <g.iarrusso.dev@gmail.com>
2021-02-14 23:12:59 -0500

    clean af boi

--- main_qa.py
+++ main_qa.py
@@ -1,29 +1,5 @@
-"""
-TODO
-add a dict for #:test file
-add a list of suites: PR, Daily, regression
-pseudo code:
-tests/regression = {
-    1: "test_TC1_name"
-    .
-    .
-    .
-}
-Daily = {
-    base of code that people need to run
-}
-PR = {
-    base of code to run on a PR
-}
-can expand to targeted regression i.e. movement, attacking, scavenging
-
-run each test
-"""
 import glob
 from subprocess import call
 
-txtfiles = []
 for file in glob.glob("UT*.py"):
-    txtfiles.append(file)
     call(["python", file])
-print(txtfiles)
